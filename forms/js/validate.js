de4756f63383d53b2384f92b7b9366535b2ded97
Gerfier <gerrardf7@gmail.com>
2018-05-22 10:28:23 -0500

    commenting js file

--- forms/js/validate.js
+++ forms/js/validate.js
@@ -7,20 +7,21 @@ var validate = function () {
     var testing_email = "adopt@mail.com";
     var testing_password = "happypet";
 
-
+    //display message if not input provided
     if (email == '' && password == '') {
         document.getElementById('message').innerHTML = 'Please fill out both spaces';
     }
-    else
+    else //check that both spaces are filled
         if ((email == '' && password == testing_password) || (email == testing_email && password == '')) {
             document.getElementById('message').innerHTML = 'Please fill out both spaces';
         }
 
-        else
+        else    //return success if email and password matched the correct ones
             if (email == testing_email && password == testing_password) {
                 document.getElementById('message').innerHTML = 'Success!';
             }
             else {
+                //if none of the above occurs, display error message
                 document.getElementById('message').innerHTML = 'Your password or username are incorrect!';
             }
 
