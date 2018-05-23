41764a12abdb71ee7feb45747022f275bfaace6f
Gerfier <gerrardf7@gmail.com>
2018-05-23 13:31:36 -0500

    using alert
    -alert for error messages

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Idea came from "The Web Developer Bootcamp" course from Udemy.com
 Only modified to a different startup company with similar structure.
 
 You can run it using this link: https://cdn.rawgit.com/Gerfier/PetUs-Bootstrap/dcde6a10/index.html
+
+To Fix:
+-Issue when opening in Firefox and Microsoft Edge, Opera and Chrome displays correctly
+    The index file appears blank with only the navbar links and within seconds it displays the page correctly
--- forms/js/validate.js
+++ forms/js/validate.js
@@ -9,20 +9,20 @@ var validate = function () {
 
     //display message if not input provided
     if (email == '' && password == '') {
-        document.getElementById('message').innerHTML = 'Please fill out both spaces';
+        alert('Please fill out both spaces');
     }
     else //check that both spaces are filled
         if ((email == '' && password == testing_password) || (email == testing_email && password == '')) {
-            document.getElementById('message').innerHTML = 'Please fill out both spaces';
+            alert('Please fill out both spaces');
         }
 
         else    //return success if email and password matched the correct ones
             if (email == testing_email && password == testing_password) {
-                document.getElementById('message').innerHTML = 'Success!';
+                alert('Success!');
             }
             else {
                 //if none of the above occurs, display error message
-                document.getElementById('message').innerHTML = 'Your password or username are incorrect!';
+                alert('Your password or username are incorrect!');
             }
 
 
